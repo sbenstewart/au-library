e398a41161dcafd4c4684c4fd6f059423e14c324
sbenstewart <sbenstewart@gmail.com>
2019-02-19 21:47:58 +0530

    database updates

--- database/library.sql
+++ database/library.sql
@@ -3,13 +3,11 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 18, 2019 at 05:36 PM
+-- Generation Time: Feb 19, 2019 at 04:17 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -30,6 +28,7 @@ USE `library`;
 -- Table structure for table `book`
 --
 -- Creation: Feb 12, 2019 at 11:53 AM
+-- Last update: Feb 19, 2019 at 03:25 PM
 --
 
 DROP TABLE IF EXISTS `book`;
@@ -42,13 +41,19 @@ CREATE TABLE `book` (
   `remaining` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`id`, `isbn`, `name`, `author`, `count`, `remaining`) VALUES
+(1, 'test', 'test', 'test', 12, 12);
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `config`
 --
 -- Creation: Feb 18, 2019 at 05:27 PM
--- Last update: Feb 18, 2019 at 05:31 PM
 --
 
 DROP TABLE IF EXISTS `config`;
@@ -73,6 +78,7 @@ INSERT INTO `config` (`key`, `value`) VALUES
 -- Table structure for table `history`
 --
 -- Creation: Feb 18, 2019 at 05:35 PM
+-- Last update: Feb 19, 2019 at 03:27 PM
 --
 
 DROP TABLE IF EXISTS `history`;
@@ -85,12 +91,22 @@ CREATE TABLE `history` (
   `fine` int(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Triggers `history`
+--
+DROP TRIGGER IF EXISTS `dec-remaining`;
+DELIMITER $$
+CREATE TRIGGER `dec-remaining` AFTER INSERT ON `history` FOR EACH ROW UPDATE book SET `remaining` = `remaining` + 1 WHERE ID = NEW.BOOKID
+$$
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `issued`
 --
--- Creation: Feb 18, 2019 at 05:34 PM
+-- Creation: Feb 19, 2019 at 03:31 PM
+-- Last update: Feb 19, 2019 at 03:31 PM
 --
 
 DROP TABLE IF EXISTS `issued`;
@@ -100,8 +116,27 @@ CREATE TABLE `issued` (
   `bookid` int(11) NOT NULL,
   `issuedate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `returndate` date NOT NULL,
-  `fine` int(100) DEFAULT NULL
+  `fine` int(100) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `issued`
+--
+
+INSERT INTO `issued` (`id`, `userid`, `bookid`, `issuedate`, `returndate`, `fine`) VALUES
+(1, 1, 1, '2019-02-19 15:31:26', '2019-02-19', 0);
+
+--
+-- Triggers `issued`
+--
+DROP TRIGGER IF EXISTS `history`;
+DELIMITER $$
+CREATE TRIGGER `history` AFTER DELETE ON `issued` FOR EACH ROW -- Edit trigger body code below this line. Do not edit lines above this one
+BEGIN
+INSERT into history VALUES (old.id,old.userid,old.bookid,old.issuedate,old.returndate,old.fine);
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -109,6 +144,7 @@ CREATE TABLE `issued` (
 -- Table structure for table `user`
 --
 -- Creation: Feb 12, 2019 at 11:50 AM
+-- Last update: Feb 19, 2019 at 03:24 PM
 --
 
 DROP TABLE IF EXISTS `user`;
@@ -123,6 +159,13 @@ CREATE TABLE `user` (
   `mail` varchar(40) NOT NULL,
   `phone` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `reg-num`, `name`, `password`, `course`, `dept`, `year`, `mail`, `phone`) VALUES
+(1, 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test');
 
 --
 -- Indexes for dumped tables
@@ -164,25 +207,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `history`
 --
 ALTER TABLE `history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `issued`
 --
 ALTER TABLE `issued`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id for db access';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id for db access', AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -194,7 +237,6 @@ ALTER TABLE `user`
 ALTER TABLE `issued`
   ADD CONSTRAINT `issued_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `issued_ibfk_2` FOREIGN KEY (`bookid`) REFERENCES `book` (`id`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
