0d5f61389a103611d22e7b3639d12beac5391a3a
sbenstewart <sbenstewart@gmail.com>
2019-02-24 10:57:18 +0530

    borrowed books made polished

--- docs/assets/php/borrowedbook.php
+++ docs/assets/php/borrowedbook.php
@@ -13,7 +13,8 @@ try {
 
       /* Check the number of rows that match the SELECT statement */
       if ($res->fetchColumn() > 0) {
-        foreach ($conn->query("SELECT book.isbn,book.name,book.author,issued.returndate,book.remaining FROM book LEFT JOIN issued ON book.id = issued.bookid AND issued.bookid = (SELECT bookid FROM issued WHERE userid = (SELECT id from user where reg='$name'))") as $row)
+
+        foreach ($conn->query("SELECT book.isbn,book.name,book.author,history.returndate FROM history LEFT JOIN book ON book.id = history.bookid AND history.bookid = (SELECT bookid FROM history WHERE userid = (SELECT id from user where reg='$name'))") as $row)
         {
 
           echo '<tbody>';
@@ -32,10 +33,35 @@ try {
           $temp2 = $row['returndate'];
           echo $temp2;
           echo "</td><td>";
-          $remaining2 = $row['remaining'];
           $availability = 'Yes';
-          if($remaining2==0)
-          {$availability='No';}
+          echo $availability;
+          echo "</td></tr></tbody>";
+          $count = $count+1;
+
+
+          /*session is started if you don't write this line can't use $_Session  global variable*/
+        }
+
+        foreach ($conn->query("SELECT book.isbn,book.name,book.author,issued.returndate FROM issued LEFT JOIN book ON book.id = issued.bookid AND issued.bookid = (SELECT bookid FROM issued WHERE userid = (SELECT id from user where reg='$name'))") as $row)
+        {
+
+          echo '<tbody>';
+          echo "<tr><th scope='row'>";
+          echo $count;
+          echo "</th><td>";
+          $isbn2 = $row['isbn'];
+          echo $isbn2;
+          echo "</td><td>";
+          $name2 = $row['name'];
+          echo $name2;
+          echo "</td><td>";
+          $author2 = $row['author'];
+          echo $author2;
+          echo "</td><td>";
+          $temp2 = $row['returndate'];
+          echo $temp2;
+          echo "</td><td>";
+          $availability = 'No';
           echo $availability;
           echo "</td></tr></tbody>";
           $count = $count+1;
