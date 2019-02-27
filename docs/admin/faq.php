baefcbd6d7de30a516e75b6f71e5be46814d15f0
sbenstewart <sbenstewart@gmail.com>
2019-02-27 20:35:27 +0530

    logout feature

--- docs/admin/faq.php
+++ docs/admin/faq.php
@@ -113,8 +113,7 @@
                             <img class="avatar user-thumb" src="assets/images/author/avatar.png" alt="avatar">
                             <h4 class="user-name dropdown-toggle" data-toggle="dropdown">Admin <i class="fa fa-angle-down"></i></h4>
                             <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#">Details</a>
-                                <a class="dropdown-item" href="#">Log Out</a>
+                                <a class="dropdown-item" href="logout.php">Log Out</a>
                             </div>
                         </div>
                     </div>
