3276d7f5d16cdbd7e5764b92efa75f65a0380ecf
sbenstewart <sbenstewart@gmail.com>
2019-03-13 11:55:00 +0530

    donated by php

--- docs/admin/assets/php/insertbook.php
+++ docs/admin/assets/php/insertbook.php
@@ -11,7 +11,7 @@ $price2 = $_POST['price1'];
 $subject2 = $_POST['subject1'];
 $department2 = $_POST['department1'];
 $row2 = $_POST['row1'];
-
+$donate2 = $_POST['donate1'];
 
 
 
@@ -40,7 +40,7 @@ try {
         }
 
 
-  $count = $conn->exec("INSERT INTO book (isbn,name,author,count,remaining,publisher,edition,price,subject,reference,department,row) VALUES ('$bookid2','$name2','$author2',1,1,'$publisher2','$edition2',$price2,'$subject2','$count2','$department2','$row2')");
+  $count = $conn->exec("INSERT INTO book (isbn,name,author,count,remaining,publisher,edition,price,subject,reference,department,row,donatedby) VALUES ('$bookid2','$name2','$author2',1,1,'$publisher2','$edition2',$price2,'$subject2','$count2','$department2','$row2','$donate2')");
   echo "Book has been inserted.";
 
 
