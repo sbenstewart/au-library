11adaa0749eb1b45792a359ade95bebdebb38f45
sbenstewart <sbenstewart@gmail.com>
2019-03-13 14:11:28 +0530

    donated books

---
+++ docs/donated-books.php
@@ -0,0 +1,202 @@
+<?php session_start();  ob_start();
+    if (!isset($_SESSION['reg'])) {
+    header('location:index.html');
+    echo "Must redirect";
+    exit(); // <-- terminates the current script
+  }
+// close the php tag and write your HTML :)
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Borrowed Books</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/metisMenu.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/slicknav.min.css">
+    <!-- amchart css -->
+    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+    <!-- others css -->
+    <link rel="stylesheet" href="assets/css/typography.css">
+    <link rel="stylesheet" href="assets/css/default-css.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <!-- modernizr css -->
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- page container area start -->
+    <div class="page-container">
+        <!-- sidebar menu area start -->
+        <div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <p>LIBRARY</p>
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+
+                            <li class="active"><a href="borrowed-books.php"><i class="fa fa-book"></i> <span>Borrowed Books</span></a></li>
+                            <li><a href="search-books.php"><i class="fa fa-search"></i> <span>Search Books</span></a></li>
+                            <li><a href="faq.php"><i class="fa fa-question"></i> <span>FAQs</span></a></li>
+                            <li><a href="about.php"><i class="fa fa-users"></i> <span>About Us</span></a></li>
+                            <li><a href="about.php#team"><i class="fa fa-github"></i> <span>Developers</span></a></li>
+
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- sidebar menu area end -->
+        <!-- main content area start -->
+        <div class="main-content">
+            <!-- header area start -->
+
+            <!-- header area end -->
+            <!-- page title area start -->
+            <div class="page-title-area">
+                <div class="row align-items-center">
+
+                    <div class="col-sm-6">
+
+                        <div class="breadcrumbs-area clearfix">
+                          <div class="nav-btn pull-left">
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                          </div>
+                            <h4 class="page-title pull-left">CEG Hostel Library</h4>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 clearfix">
+                        <div class="user-profile pull-right">
+                            <img class="avatar user-thumb" src="assets/images/author/avatar.png" alt="avatar">
+                            <h4 class="user-name dropdown-toggle" data-toggle="dropdown">User <i class="fa fa-angle-down"></i></h4>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="logout.php">Log Out</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- page title area end -->
+            <div class="main-content-inner">
+                <div class="row">
+
+                  <div class="col-12 mt-5">
+                      <div class="card">
+                          <div class="card-body">
+                              <h3 class="header-title">Search details</h3>
+                              <label> Click the button below to load the books you donated. We really value your privacy.</label>
+                              <br>
+
+                              <button type="submit" class="btn btn-primary mt-4 pr-4 pl-4" onclick="donatedBook()">View Donated books.</button>
+
+                          </div>
+                      </div>
+                  </div>
+                    <!-- table primary start -->
+                    <div class="col-lg-12 mt-5">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="header-title">History</h4>
+                                <div class="single-table">
+                                    <div class="table-responsive">
+                                        <table class="table text-center">
+                                            <thead class="text-uppercase bg-primary">
+                                                <tr class="text-white">
+                                                    <th scope="col">S. No</th>
+                                                    <th scope="col">Book ID</th>
+                                                    <th scope="col">Name</th>
+                                                    <th scope="col">Author</th>
+                                                    <th scope="col">Date of Return</th>
+                                                    <th scope="col">Returned</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="tablebody"></tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- table primary end -->
+                    <!-- table success start -->
+
+                    <!-- table success end -->
+                    <!-- table info start -->
+
+                    <!-- table info end -->
+                    <!-- table warning start -->
+
+                    <!-- table warning end -->
+                    <!-- table danger start -->
+
+                    <!-- table danger end -->
+                    <!-- table dark start -->
+
+                    <!-- table dark end -->
+                    <!-- table light start -->
+
+                    <!-- table light end -->
+                    <!-- table secondary start -->
+
+                    <!-- table secondary end -->
+                    <!-- Contextual Classes start -->
+
+                    <!-- Contextual Classes end -->
+                    <!-- Contextual Classes start -->
+
+                    <!-- Contextual Classes end -->
+                </div>
+            </div>
+        </div>
+        <!-- main content area end -->
+        <!-- footer area start-->
+        <footer>
+            <div class="footer-area">
+                <p>Made with <i class="fa fa-rocket"></i> in <a href="http://sbenstewart.in">Chennai</a>.</p>
+            </div>
+        </footer>
+        <!-- footer area end-->
+    </div>
+    <!-- page container area end -->
+    <!-- jquery latest version -->
+    <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <!-- bootstrap 4 js -->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/metisMenu.min.js"></script>
+    <script src="assets/js/jquery.slimscroll.min.js"></script>
+    <script src="assets/js/jquery.slicknav.min.js"></script>
+    <script type='text/javascript' src='assets/js/main.js'></script>
+
+    <!-- others plugins -->
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
